490359e840cbed10ff878be97f796c3f1aede366
student-apnacollege-tech <pradyot12345@gmail.com>
2025-05-08 09:53:13 +0530

    Add files via upload

---
+++ web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/',[ProductController::class,'index'])->name('products.index');
+Route::get('/products/create',[ProductController::class,'create'])->name('products.create');
+Route::post('/products/store',[ProductController::class,'store'])->name('products.store');
+Route::get('products/{id}/edit',[ProductController::class,'edit']);
+Route::put('products/{id}/update',[ProductController::class,'update']);
+Route::delete('products/{id}/delete',[ProductController::class,'destroy']);
+
